08fc0b2e08b6f372d02b57adad4b54c4f303ffe3
Anonimo <software.newspace@gmail.com>
2021-10-19 15:58:32 -0500

    Termianando Taller N3

--- Taller-N3/moda.js
+++ Taller-N3/moda.js
@@ -1,16 +1,5 @@
 const lista1 = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    2,
-    4,
-    6,
-    2,
-    1,
-    2,
-    1,
+    1, 2, 3, 4, 5, 2, 4, 6, 2, 1, 2, 1,
 ];
 
 const lista1Cound = {};
@@ -18,11 +7,17 @@ const lista1Cound = {};
 lista1.map(
     function(elemento) {
         if(lista1Cound[elemento]) {
-            lista1Cound[elemento] += 1;
+                lista1Cound[elemento] += 1;
         }else{
             lista1Cound[elemento] = 1;
         }
     }
 );
 
-const lista1Array = Object.entries();
+const lista1Array = Object.entries(lista1Cound).sort(
+    function(elementoA, elementoB) {
+        return elementoA[1] - elementoB[1];
+    }
+);
+
+const moda = lista1Array[lista1Array.length - 1];
