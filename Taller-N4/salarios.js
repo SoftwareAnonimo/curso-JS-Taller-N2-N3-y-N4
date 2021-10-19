d4fef622b645e2c86e6a3c5b769ef6c2dd56ba0b
Anonimo <software.newspace@gmail.com>
2021-10-19 17:15:55 -0500

    Terminando los Salarios

---
+++ Taller-N4/salarios.js
@@ -0,0 +1,100 @@
+const perú = [];
+perú.push({
+    name: "Juan",
+    salary: 1000,
+});
+
+perú.push({
+    name: "Lucas",
+    salary: 1500,
+});
+
+perú.push({
+    name: "Jose",
+    salary: 1450,
+});
+
+perú.push({
+    name: "Paolo",
+    salary: 1100,
+});
+
+perú.push({
+    name: "Arturo",
+    salary: 1200,
+});
+
+perú.push({
+    name: "Ana",
+    salary: 1250,
+});
+
+perú.push({
+    name: "Camila",
+    salary: 1050,
+});
+
+perú.push({
+    name: "Luisa",
+    salary: 950,
+});
+
+perú.push({
+    name: "Alicia",
+    salary: 1800,
+});
+
+perú.push({
+    name: "Mike",
+    salary: 2000,
+});
+
+perú.push({
+    name: "Alfredo",
+    salary: 1550,
+});
+
+perú.push({
+    name: "Samuel",
+    salary: 2000,
+});
+
+perú.push({
+    name: "Alejandro",
+    salary: 1200,
+});
+
+perú.push({
+    name: "Alberto",
+    salary: 950,
+});
+
+perú.push({
+    name: "Liam",
+    salary: 1000,
+});
+
+perú.push({
+    name: "Jino",
+    salary: 1300,
+});
+
+perú.push({
+    name: "Martin",
+    salary: 1350,
+});
+
+perú.push({
+    name: "Fernanda",
+    salary: 1400,
+});
+
+perú.push({
+    name: "Violeta",
+    salary: 990,
+});
+
+perú.push({
+    name: "Veky",
+    salary: 1700,
+});
